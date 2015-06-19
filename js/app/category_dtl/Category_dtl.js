a889274a019a088f8449ebe155840e06cd3c6b39
hykim <hykim@gnewsoft.com>
2015-06-19 17:58:08 +0900

    -

--- js/app/category_dtl/Category_dtl.js
+++ js/app/category_dtl/Category_dtl.js
@@ -32,39 +32,32 @@ var cartFocus = 0;
 //*************************************************
 /** 
  *  현재 리스트 Focus 위치
- *  0 : 상세 카테고리 열1
- *  1 : 상세 카테고리 열2
- *  2 : 상세 카테고리 열3
+ *  0 : 상세 카테고리
  *  3 : 앞으로 이동하여 다른 코너로 이동할수 있습니다.
  *  4 : 당일 판매현장 필요?
  **/
 var currentFocusList = 0;
+
 /**
- * 상세카테고리 Focus 위치 1열
- * 상세카테고리 Focus 위치 2열
- * 상세카테고리 Focus 위치 3열
- * 0 : 
- * 1 : 
- * 2 :
- * 3 :
-  **/
-var currentFocusDtl1 = 0;
-var currentFocusDtl2 = 0;
-var currentFocusDtl3 = 0;
+ * 상세카테고리에서 행열의 위치 
+ * horizonFocus    = 가로 0 1 2
+ * verticalFocus   = 세로 0 1 2
+ * currentFocusDtl = 현재 위치 012
+ *                             345
+ *                             678
+ * productList     = 상품 배열
+ **/
+var horizonFocus  = 0;
+var verticalFocus = 0;
+var currentFocusDtl = 0;
+var productList   = new Array();
+
 /**
  * 현재 상세카테고리 화살표 button Focus 위치
  * 0 : 아래쪽
  * 1 : 위쪽
  **/
 var currentFocusBtn = 0;
-
-
-/**
- * 현재 상세카테고리 Focus 위치
- * 0 : 사과/배
- * 1 : 참외/토마토...
- **/
-var currentFocusDtl = 0;
 
 /** 
  * 현재 상세카테고리의 페이지
@@ -80,22 +73,11 @@ var currentFocusDtlPage = 0;
  var prevPageYN = false;
  var nextPageYN = false;
 
-/** 
- *  현재 카테고리 Focus 위치
- *  0 : 사람 모양
- *  1 : 음성 인식
- *  2 : 동그란 사람 얼굴
- *  3 : 쇼퍼's Bag
- *  4 : 과일... 
- **/    
-var currentFocusMenu = 3;
+/**
+ * 상세카테고리 : 확인/상세/담기 
+ **/
+var btnokfill = '<img src="../images/btn_ok_fill.png"/>';
 
-
-
- /**
-  * 상세카테고리 : 확인/상세/담기 
-  **/
-  var btnokfill = '<img src="../images/btn_ok_fill.png"/>';
 
 global.onload = function() {
     global.Category_dtl = Gigamart.app.category_dtl.Category_dtl.create();
@@ -117,19 +99,7 @@ App.defineClass('Gigamart.app.category_dtl.Category_dtl', {
 
     },
     init: function() {
-        //addClass(document.querySelectorAll('category_menu')[currentFocusMenu], 'focus');
-
         var me = this;
-        //$('ul[name="li_discount"] li').eq(currentFocusDtl1).addClass('focus');        
-        $('li[name="li_discount1"]').eq(currentFocusDtl1).addClass('focus');
-        
-
-
         global.stbService = Gigamart.app.category_dtl.STBService.create(EventBus);
-
-
-        //me.videoObject = document.querySelector('#cate_bag');
-        //global.playerPanel = Gigamart.components.player.PlayerPanel.create();
-
     }
 });
